4885fbe77f263cb153d338a6ca8f31ee06b861fa
Leimberger <kleimberger@gmail.com>
2022-09-09 11:58:04 -0700

    Adding code for compiling and plotting results

--- code/helper_functions/Plotting_helper_functions.R
+++ code/helper_functions/Plotting_helper_functions.R
@@ -3,27 +3,42 @@
 ############################
 #Plot estimated marginals means for:
 #control pre, control post, treatment pre, treatment post
-make_interaction_plot <- function(ggeffects_df, yvar, ymax){
+make_interaction_plot <- function(ggeffects_df, yvar, ymin, ymax, ybreak, yaccuracy, as_percent = TRUE){
   
   #Colors and shapes
   colors <- c("#0E0D37", "#BA0022") #blue, red
   shapes <- c(16, 17) #circle, triangle (filled)
   
   #Label for y-axis
-  # if(yvar == "H2"){ylabel <- c(expression(atop("Network specialization", paste("("*italic(H[2])*"')"))))}
-  # if(yvar == "d"){ylabel <-  c(expression(atop("Species specialization", paste("(mean "*italic(d)*"')"))))}
-  # if(yvar == "species.specificity.index"){ylabel <- c(expression(atop("Species specialization", paste("(mean SSI)"))))}
-  
-  if(yvar == "H2"){ylabel <- c(expression(paste(italic(H[2])*"'")))}
-  if(yvar == "d"){ylabel <-  c(expression(paste("mean "*italic(d)*"'")))}
-  if(yvar == "species.specificity.index"){ylabel <- c("mean SSI")}
-  if(yvar == "num_morphotypes"){ylabel <- c("# morphotypes")}
+  if(yvar == "sightings"){ylabel <- c("Visitation rate (visits/day)")}
+  if(yvar == "num_birds"){ylabel <-  c("# birds captured")}
+  if(yvar == "prop_time_in_patch"){ylabel <- c("Proportion time in focal area")}
+  if(yvar == "prop_with_tubes"){ylabel <- c("Proportion flowers pollinated")}
+  if(yvar == "relative_mass"){ylabel <- c("Relative body mass")}
   
   #Group/legend labels
   group_labels <- c("control" = "Control", "treatment" = "Treatment")
   legend_label <- c(expression(paste(italic("Heliconia "), "removal", sep = "")))
  
-  #Plotting by hand for increased customizability
+  if(as_percent == TRUE){
+    
+    if(yvar == "prop_with_tubes" | yvar == "prop_time_in_patch"){
+      
+      ggeffects_df <- ggeffects_df %>%
+        mutate(predicted = predicted * 100, 
+               conf.low = conf.low * 100,
+               conf.high = conf.high * 100)
+      
+      ymax <- ymax * 100
+      ymin <- ymin * 100
+      
+      if(yvar == "prop_time_in_patch"){ylabel <- c("% time in focal area")}
+      if(yvar == "prop_with_tubes"){ylabel <- c("% flowers pollinated")}
+
+      }
+    
+  }
+  
   plot <- ggeffects_df %>%
     mutate(group = factor(group, levels = c("control", "treatment"), labels = c("Control", "Treatment"))) %>%
     mutate(x = factor(x, levels = c("pre", "post"), labels = c("Pre", "Post"))) %>%
@@ -33,11 +48,18 @@ make_interaction_plot <- function(ggeffects_df, yvar, ymax){
       geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(width = 0.25), width = 0.00, size = 1) +
       theme_bw(base_size = 18) +
       theme(legend.position = "bottom", legend.direction = "horizontal", panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-      scale_y_continuous(labels = scales::number_format(accuracy = 0.1, decimal.mark = '.')) +
+      scale_y_continuous(limits = c(ymin, ymax), breaks = seq(from = ymin, to = ymax, by = ybreak), labels = scales::number_format(accuracy = yaccuracy)) +
       scale_color_manual(values = colors, labels = group_labels) +
       scale_shape_manual(values = shapes, labels = group_labels) +
-      labs(x = "Experimental period", y = ylabel, colour = legend_label, shape = legend_label) +
-      ylim(0, ymax)
+      labs(x = "Experimental period", y = ylabel, colour = legend_label, shape = legend_label)
+  
+  
+  if(yvar == "relative_mass"){
+    
+    plot <- plot +
+      scale_y_continuous(limits = c(0.9, 1.1), breaks = c(0.9, 1.0, 1.1), labels = scales::number_format(accuracy = 0.1))
+      
+  }
     
   return(plot)
   
@@ -47,26 +69,39 @@ make_interaction_plot <- function(ggeffects_df, yvar, ymax){
 #Make control vs. treatment plot
 ##################################
 #This plot is similar to interaction plot, but for response variables not calculated at the level of experimental period (control vs. treatment only)
-make_control_vs_treatment_plot <- function(ggeffects_df, yvar, ymax){
+make_control_vs_treatment_plot <- function(ggeffects_df, yvar, ymin, ymax, ybreak, yaccuracy, as_percent){
   
   #Label for y-axis
-  if(yvar == "WN"){ylabel <- c("Total")}
-  if(yvar == "ST"){ylabel <-  c("Species gain or loss")}
-  if(yvar == "OS"){ylabel <- c("Among shared species")}
+  if(yvar == "recap_rate"){ylabel <- c("Proportion of birds recaptured")}
+  
+  if(as_percent == TRUE){
+    
+    if(yvar == "recap_rate"){
+      
+      gggeffects_df <- ggeffects_df %>%
+        mutate(predicted = predicted * 100, 
+               conf.low = conf.low * 100,
+               conf.high = conf.high * 100)
+      
+      ymax <- ymax * 100
+      ymin <- ymin * 100
+      ylabel <- c("Probability of recapture (%)")}
+    
+  }
   
   plot <- ggeffects_df %>%
     mutate(x = factor(x, levels = c("control", "treatment"), labels = c("Control", "Treatment"))) %>%
-    ggplot(data = ., aes(x = x, y = predicted)) + 
+    ggplot(data = ., aes(x = x, y = predicted)) + #Multiply by 100 to get percent
     geom_point(aes(x = x, y = predicted), position = position_dodge(width = 0.25), size = 4) +
     geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(width = 0.25), width = 0.0, size = 1) +
+    scale_y_continuous(limits = c(ymin, ymax), breaks = seq(from = ymin, to = ymax, by = ybreak), labels = scales::number_format(accuracy = yaccuracy)) +
     theme_bw(base_size = 20) +
     theme(legend.position = "none",
           legend.direction = "horizontal",
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           plot.title = element_text(hjust = 0.5)) +
-    labs(x = "", y = ylabel, shape = "", color = "") +
-    ylim(0, ymax)
+    labs(x = "", y = ylabel, shape = "", color = "")
   
   return(plot)
   
@@ -82,19 +117,19 @@ make_contrast_plot <- function(contrasts_df, xvar, shading = "none"){
   legend_text_labels <- c(expression("All species"), expression(paste(italic("Heliconia "), "specialists", sep = "")))
   
   plot_data <- contrasts_df %>%
-    mutate(order = 1:length({{ xvar }}))
+    mutate(order = seq.int(nrow(.)))
   
   if(shading == "below"){
     
     plot <- plot_data %>%
-      ggplot(data = ., aes(x = forcats::fct_reorder(.data[[xvar]], order), y = ratio, shape = bird_group)) +
+      ggplot(data = ., aes(x = forcats::fct_reorder(.data[[xvar]], order), y = ratio)) +
         geom_rect(xmax = Inf, xmin = -Inf, ymax = 1, ymin = 0, fill = "grey90", alpha = 1)
   }
   
   if(shading == "above"){
     
     plot <- plot_data %>%
-      ggplot(data = ., aes(x = forcats::fct_reorder(.data[[xvar]], order), y = ratio, shape = bird_group)) +
+      ggplot(data = ., aes(x = forcats::fct_reorder(.data[[xvar]], order), y = ratio)) +
         geom_rect(xmax = Inf, xmin = -Inf, ymax = Inf, ymin = 1, fill = "grey90", alpha = 1)
     
   }
@@ -102,13 +137,31 @@ make_contrast_plot <- function(contrasts_df, xvar, shading = "none"){
   if(shading == "none"){
     
     plot <- plot_data %>%
-      ggplot(data = ., aes(x = forcats::fct_reorder(.data[[xvar]], order), y = ratio, shape = bird_group))
+      ggplot(data = ., aes(x = forcats::fct_reorder(.data[[xvar]], order), y = ratio))
     
   }
   
-  plot <- plot +
-      geom_point(position = position_dodge(.5), size = 3) +
+  #For pollination, will not have separate bird groups
+  if("pollen_tubes" %in% contrasts_df$analysis){
+    
+    plot <- plot +
+      geom_point(position = position_dodge(.5), size = 3) + 
       geom_errorbar(position = position_dodge(.5), aes(ymax = upper.CL, ymin = lower.CL), width = 0.25, size = 1) + # error bars show 95% confidence intervals
+      geom_hline(yintercept = 1, color = "black", linetype = "dashed", alpha = 0.8) + # add a line at 1 (no effect)
+      theme_bw(base_size = 18) +
+      scale_shape_manual(values = c(16)) +
+      theme(legend.position = "top", legend.justification = "center", legend.text = element_text(size = 18), legend.title = element_text(size = 18),
+            panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      labs(x = "", y = "Ratio")
+    
+  }
+  
+  #For hummingbird-centric varaibles, will have separate bird groups
+  if(!("pollen_tubes" %in% contrasts_df$analysis)){
+    
+    plot <- plot +
+      geom_point(aes(shape = bird_group), position = position_dodge(.5), size = 3) +
+      geom_errorbar(aes(ymax = upper.CL, ymin = lower.CL, group = bird_group), position = position_dodge(.5), width = 0.25, size = 1) + # error bars show 95% confidence intervals
       geom_hline(yintercept = 1, color = "black", linetype = "dashed", alpha = 0.8) + # add a line at 1 (no effect)
       theme_bw(base_size = 18) +
       scale_shape_manual(values = c(16, 17), labels = legend_text_labels, limits = c("all_spp", "greh_visa")) +
@@ -116,6 +169,8 @@ make_contrast_plot <- function(contrasts_df, xvar, shading = "none"){
             panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
       labs(x = "", y = "Ratio", shape = "Bird group")
     
+  }
+  
   return(plot)
   
 }
