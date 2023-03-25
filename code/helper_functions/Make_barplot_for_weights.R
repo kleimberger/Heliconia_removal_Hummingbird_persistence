3dec6ff685ebb2ba6c3903b1bd32f690d97bbd49
Leimberger <kleimberger@gmail.com>
2023-03-25 14:17:17 -0700

    making multipanel figure for main text - visit rates and percent calories removed

--- code/helper_functions/Make_barplot_for_weights.R
+++ code/helper_functions/Make_barplot_for_weights.R
@@ -1,28 +1,28 @@
-make_barplot <- function(data, xvar, yvar, xlabel, ylabel, highlight_column, titlelabel, flip_axes = FALSE){
+make_barplot <- function(data, xvar, yvar, xlabel, ylabel, highlight_column, titlelabel = "", flip_axes = FALSE, text_size){
   
   plot <- data %>% 
-    ggplot(aes(x = reorder(.data[[xvar]], -.data[[yvar]]), y = .data[[yvar]], fill = .data[[highlight_column]]))+
-    geom_bar(stat = "identity", width = c(0.6))+
-    labs(x = xlabel, y = ylabel, title = titlelabel)+
-    theme_bw(base_size = 20) + 
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 16, face = "italic"),
+    ggplot(aes(x = forcats::fct_reorder(.data[[xvar]], .data[[yvar]]), y = .data[[yvar]], fill = .data[[highlight_column]])) +
+    geom_bar(stat = "identity", width = c(0.6)) +
+    labs(x = xlabel, y = ylabel, title = titlelabel) +
+    theme_bw(base_size = text_size) + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = text_size * 0.75, face = "italic"),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank())+
-    scale_fill_manual(values = c("dark grey", "#BA0022"), guide = "none")
+    scale_fill_manual(values = c("grey60", "#BA0022"), guide = "none")
   
   if(flip_axes == "TRUE"){
     
     plot <- data %>% 
-      ggplot(aes(x = reorder(.data[[xvar]], .data[[yvar]]), y = .data[[yvar]], fill = .data[[highlight_column]]))+
+      ggplot(aes(x = forcats::fct_reorder(.data[[xvar]], .data[[yvar]]), y = .data[[yvar]], fill = .data[[highlight_column]])) +
       geom_bar(stat = "identity", width = c(0.6)) +
-      labs(x = xlabel, y = ylabel, title = titlelabel)+
-      theme_bw(base_size = 20)+ #Increase text size for AOS talk
-      theme(axis.text.y = element_text(size = 16, face = "italic"),
+      labs(x = xlabel, y = ylabel, title = titlelabel) +
+      theme_bw(base_size = text_size) +
+      theme(axis.text.y = element_text(size = text_size * 0.75, face = "italic"),
             panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank())+
-      scale_fill_manual(values = c("dark grey", "#BA0022"), guide = "none") +
+            panel.grid.minor = element_blank()) +
+      scale_fill_manual(values = c("grey60", "#BA0022"), guide = "none") +
       coord_flip() +
-      scale_y_continuous(expand = c(0, 0.05)) #Make bars go closer to y axis
+      scale_y_continuous(expand = c(0, 0.01)) #Make bars go closer to y axis
   }
   
   #Italicize Heliconia
@@ -34,4 +34,5 @@ make_barplot <- function(data, xvar, yvar, xlabel, ylabel, highlight_column, tit
   }
   
   return(plot)
+  
 }
